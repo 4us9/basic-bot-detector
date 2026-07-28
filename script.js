fbc38693c97c50bbb46401cc1bc700d587112a30
Pang <austinpang19@gmail.com>
2026-07-27 21:12:10 -0600

    Implemented function to look for browser behavior and seeing if matchign User-Agent browser info.

--- script.js
+++ script.js
@@ -21,6 +21,48 @@ function detectBot() {
         noLanguages: (navigator.languages?.length || 0) === 0, 
 
     } 
+}
+
+/**
+ * Verify that the info from User Agent behaves like it. JS environment for that User-Agent
+ * should match. If Chrome, JS env and more should look like Chrome
+ * 
+ * @return boolean (true or false) if a browser is suspicious 
+ */
+function detectInconsistentEval() {
+
+    //Different browsers format string differently.
+    let length = eval.toString().length
+
+    //Chrome always returns 33; FireFox 37
+    let userAgent = navigator.userAgent.toLowerCase()
+    let browser
+
+    if (userAgent.indexOf('edg/') !== -1) {
+        browser = 'edge'
+    } else if (userAgent.indexOf('trident') !== -1 || userAgent.indexOf('msie') !== -1) {
+    
+        browser = 'internet_explorer'
+    } else if (userAgent.indexOf('firefox') !== -1) {
+        browser = 'firefox'
+
+    } else if (userAgent.indexOf('opera') !== -1 || userAgent.indexOf('opr') !== -1) {
+        browser = 'opera'
+    } else if (userAgent.indexOf('chrome') !== -1) {
+        browser = 'chrome'
+    } else if (userAgent.indexOf('safari') !== -1) {
+        browser = 'safari'
+    } else {
+        browser = 'unknown'
+  }
+
+  if (browser === 'unknown') return false
+
+  return (
+    (length === 33 && !['chrome', 'opera', 'edge'].includes(browser)) ||
+    (length === 37 && !['firefox', 'safari'].includes(browser)) ||
+    (length === 39 && !['internet_explorer'].includes(browser))
+  )
 
 }
 
